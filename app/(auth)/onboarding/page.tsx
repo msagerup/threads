0a71d81d1336774fff9d4d7dc53de8b5e2bc1b10
Magnus <magnus.sagerup@gmail.com>
2023-08-09 11:46:30 +0200

    initial

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+async function Page() {
+    return (
+        <main>
+            <h1>Onboarding</h1>
+            <p>Let's get you set up.</p>
+        </main>
+    )   
+}
+
+export default Page
