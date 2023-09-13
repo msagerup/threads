c774ab7662b5e98b43e78460df60de0d01234054
Magnus Sagerup <magnus.sagerup@gmail.com>
2023-09-13 22:31:20 +0200

    small refactor

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -2,11 +2,28 @@ import { Thread } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
+const images = [
+  { image: "/assets/heart-gray.svg", alt: "heart icon" },
+  { image: "/assets/reply.svg", alt: "reply icon" },
+  { image: "/assets/repost.svg", alt: "repost icon" },
+  { image: "/assets/share.svg", alt: "share icon" },
+].map(({image, alt}) => {
+  return (
+    <Image
+      key={image}
+      className='cursor-pointer object-contain'
+      src={image}
+      alt={alt}
+      width={24}
+      height={24}
+    />
+  );
+});
+
 const ThreadCard = ({ thread }: any) => {
   //Constants
   const { text, author, replies, updatedAt, createdAt } = thread;
 
-  console.log("****", thread);
 
   return (
     <article className='flex w-full flex-col rounded-lg bg-dark-2 p-7'>
@@ -34,36 +51,9 @@ const ThreadCard = ({ thread }: any) => {
             </Link>
             <p className='mt-2 text-small-regular text-light-2'>{text}</p>
             <div className='mt-5 flex flex-col gap-3'>
-              <div className='flex gap-3.5'>
-                <Image
-                  className='cursor-pointer object-contain'
-                  src='/assets/heart-gray.svg'
-                  alt='heart icon'
-                  width={24}
-                  height={24}
-                />
-                <Image
-                  className='cursor-pointer object-contain'
-                  src='/assets/reply.svg'
-                  alt='reply icon'
-                  width={24}
-                  height={24}
-                />
-                <Image
-                  className='cursor-pointer object-contain'
-                  src='/assets/repost.svg'
-                  alt='repost icon'
-                  width={24}
-                  height={24}
-                />
-                <Image
-                  className='cursor-pointer object-contain'
-                  src='/assets/share.svg'
-                  alt='share icon'
-                  width={24}
-                  height={24}
-                />
-              </div>
+                <div className='flex gap-3.5'>
+                    {images}
+                </div>
             </div>
           </div>
         </div>
