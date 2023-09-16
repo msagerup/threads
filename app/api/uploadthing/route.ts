3a9a20ce167f1b39b4a6aefeb99cedfc4ead5a9e
Magnus Sagerup <magnus.sagerup@gmail.com>
2023-09-16 18:19:39 +0200

    change edge region

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,3 +1,6 @@
+export const runtime = 'edge'; // 'nodejs' is the default
+export const preferredRegion = 'fra1'; // Frankfurt, Germany
+
 import { createNextRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
